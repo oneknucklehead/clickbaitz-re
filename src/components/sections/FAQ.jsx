da374325e2292250d29483bc951b31e6aeeaec7b
Zoheb <zohebcool1542@gmail.com>
2024-09-15 16:47:51 +0530

    FAQ section bug fixed

--- src/components/sections/FAQ.jsx
+++ src/components/sections/FAQ.jsx
@@ -1,43 +1,8 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-
+import faqData from "@/data/FAQdata";
 const FAQSection = () => {
-  // Define FAQ questions and answers
-  const faqData = [
-    {
-      id: "01",
-      question: "We need results immediately how do you deliver?",
-      answer:
-        "We have a history of making big things happen for the ecommerce brands that we work with and we know how to translate that historical success to work hard for your brand as well. We have the tools and the skills to help take your brand to the next level. The success of your ecommerce brand is our top priority. We use innovative campaign crafting and design techniques to make superior content that drives sales for your brand. ",
-    },
-    {
-      id: "02",
-      question: "What is your pricing structure?",
-      answer:
-        "Our pricing structure is based on the scope of work and the specific needs of your business. We offer flexible pricing to accommodate various business sizes and goals.",
-    },
-    {
-      id: "03",
-      question: "Can you deliver for our design needs?",
-      answer:
-        "Absolutely! We have a talented design team that specializes in creating bespoke, high-converting designs for all business types.",
-    },
-    {
-      id: "04",
-      question: "Do you have experience with my industry?",
-      answer:
-        "Yes, we have worked with clients from a wide range of industries and bring that experience to every new project we undertake.",
-    },
-    {
-      id: "05",
-      question: "Will your team take care of everything?",
-      answer:
-        "Our team can handle everything from strategy to execution. We’ll work with you closely to ensure every aspect of the project meets your expectations.",
-    },
-  ];
-
-  // Set state to track the selected question
   const [selectedQuestion, setSelectedQuestion] = useState(faqData[0]);
 
   return (
