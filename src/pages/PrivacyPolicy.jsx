d7cb4763ee3a23812e06e0f4f249a4e998095bba
Zoheb <zohebcool1542@gmail.com>
2025-03-12 03:34:27 +0530

    privacy policy added

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import privacyContent from "@/data/privacyPolicy";
+import AboutGridItems from "@/components/custom/AboutGridItems";
+import Footer from "@/components/sections/Footer";
 
 const PrivacyPolicy = () => {
-  return <div>PrivacyPolicy</div>;
+  return (
+    <div>
+      <div className="bg-black text-white">
+        <h1 className="text-theme text-6xl text-center py-16">
+          Privacy Policy
+        </h1>
+        <div className="max-w-[1000px] mx-auto">
+          {/* <div className="min-h-screen"> */}
+          {privacyContent.map((content, index) => (
+            <AboutGridItems
+              key={index}
+              // tagBehind={"story"}
+              title={content.title}
+              description={content.description}
+            />
+          ))}
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default PrivacyPolicy;
