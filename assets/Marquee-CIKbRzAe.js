d98677b9c5e6da20521fc314e846099ae51a3a9b
Zoheb <zohebcool1542@gmail.com>
2025-06-04 17:29:08 +0530

    Updates

--- assets/Marquee-CIKbRzAe.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-YybkskQ2.js";const e=({children:s,className:r})=>t.jsx("div",{className:`mx-auto ${r}`,children:s});export{e as M};
---
+++ assets/Marquee-WD56NzDd.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DaGfcmD6.js";const e=({children:s,className:r})=>t.jsx("div",{className:`mx-auto ${r}`,children:s});export{e as M};
