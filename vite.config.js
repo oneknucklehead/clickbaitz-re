32db1efea3e4c77759b3b1fae93dad715418d4df
Zoheb <zohebcool1542@gmail.com>
2025-06-09 03:45:38 +0530

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/clickbaitz-re/",
+  base: "/",
   assetsInclude: ["**/*.JPG"],
   resolve: {
     alias: {
